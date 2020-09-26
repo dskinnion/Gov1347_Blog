39282f5e042c9f113f604794683a6d231be7b01c
Dominic Skinnion <dominicskinnion@Dominics-MacBook-Pro.local>
2020-09-25 21:53:18 -0400

    updated with pivot wider

--- scripts/3_Blog_Script_Poll.R
+++ scripts/3_Blog_Script_Poll.R
@@ -3,7 +3,20 @@
 library(tidyverse)
 
 # Load in data
+
 polls_2020 <- read_csv('data/polls_2020.csv')
 polls_2016 <- read_csv('data/polls_2020.csv')
 poll_avg_state <- read_csv('data/pollavg_bystate_1968-2016.csv')
 poll_avg <- read_csv('data/pollavg_1968-2016.csv')
+
+# Data Cleaning
+
+polls_2020 <- polls_2020 %>%
+  pivot_wider(names_from = "candidate_party", values_from = "pct") %>%
+  group_by(question_id) %>%
+  fill(DEM, REP, OTH, GRE, LIB, CON, IND, .direction = "downup")
+
+polls_2016 <- polls_2016 %>%
+  pivot_wider(names_from = "candidate_party", values_from = "pct") %>%
+  group_by(question_id) %>%
+  fill(DEM, REP, OTH, GRE, LIB, CON, IND, .direction = "downup")
