abdf517990cecc32d121be3d300b11ee9049c49b
Dominic Skinnion <dominicskinnion@Dominics-MacBook-Pro.local>
2020-09-25 21:20:03 -0400

    added data, script and blog for polling

---
+++ posts/3_Blog_Post_Poll.md
@@ -0,0 +1,7 @@
+# Polling
+
+## September 25, 2020
+
+<br>
+
+### Background
---
+++ scripts/3_Blog_Script_Poll.R
@@ -0,0 +1,9 @@
+# Load libraries
+
+library(tidyverse)
+
+# Load in data
+polls_2020 <- read_csv('data/polls_2020.csv')
+polls_2016 <- read_csv('data/polls_2020.csv')
+poll_avg_state <- read_csv('data/pollavg_bystate_1968-2016.csv')
+poll_avg <- read_csv('data/pollavg_1968-2016.csv')
