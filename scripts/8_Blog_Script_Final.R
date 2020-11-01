89cf1b53f025ebfaf29e48540e24722e0fd5d2cb
Dominic Skinnion <dominicskinnion@Dominics-MacBook-Pro.local>
2020-11-01 11:14:10 -0500

    moved post to correct location

--- scripts/8_Blog_Script_Final.R
+++ scripts/8_Blog_Script_Final.R
@@ -6,6 +6,7 @@ library(maps)
 library(loo)
 library(geofacet)
 library(broom)
+library(caret)
 library(stringr)
 
 # Load Data
@@ -97,7 +98,7 @@ pres_appr_2020 <- pres_appr_2020 %>%
   mutate(weight = 1 / days_left) %>%
   mutate(rel_weight = weight / sum(weight)) %>%
   mutate(net_approval_rw = net_approval * rel_weight) %>%
-  summarize(net_approval_weighted = sum(net_approval_rw)) %>%
+  summarize(net_approval_weighted = sum(net_approval_rw) / 100) %>%
   mutate(year = 2020) %>%
   select(year, net_approval_weighted)
 
@@ -141,7 +142,7 @@ pres_appr_hist <- pres_appr_hist %>%
   ungroup() %>%
   mutate(net_approval_rw = net_approval * rel_weight) %>%
   group_by(year) %>%
-  summarize(net_approval_weighted = sum(net_approval_rw)) %>%
+  summarize(net_approval_weighted = sum(net_approval_rw) / 100) %>%
   mutate(year = as.integer(year))
 
 pv_hist_natl <- pv_hist_natl %>%
@@ -149,14 +150,14 @@ pv_hist_natl <- pv_hist_natl %>%
   mutate(D_inc = ifelse(party == "democrat" & incumbent == "TRUE", "TRUE", "FALSE")) %>%
   mutate(R_inc = ifelse(party == "republican" & incumbent == "TRUE", "TRUE", "FALSE")) %>%
   pivot_wider(id_cols = "year", names_from = party, values_from = pv2p) %>%
-  mutate(D_pv2p = democrat,
-         R_pv2p = republican) %>%
+  mutate(D_pv2p = democrat / 100,
+         R_pv2p = republican / 100) %>%
   mutate(fmr_D_pv2p = lag(D_pv2p),
          fmr_R_pv2p = lag(R_pv2p)) %>%
   select(year, D_pv2p, R_pv2p, fmr_D_pv2p, fmr_R_pv2p)
 
-pv_hist_natl[1, 4] = 53.77380
-pv_hist_natl[1, 5] = 46.22619
+pv_hist_natl[1, 4] = 0.5377380
+pv_hist_natl[1, 5] = 0.4622619
 
 demog_2020 <- demog %>%
   filter(year == 2018) %>%
@@ -181,8 +182,149 @@ demog_hist <- demog %>%
 
 hist_natl <- full_join(pv_hist_natl, pres_poll_avg_hist_natl, by = "year") %>%
   full_join(demog_hist, by = "year") %>%
-  full_join(pres_appr_hist, by = "year")
+  full_join(pres_appr_hist, by = "year") %>%
+  mutate(R_inc = case_when(
+    year == "2016" ~ 0,
+    year == "2012" ~ 0,
+    year == "2008" ~ 0,
+    year == "2004" ~ 1,
+    year == "2000" ~ 0,
+    year == "1996" ~ 0,
+    year == "1992" ~ 1,
+    year == "1988" ~ 0,
+    year == "1984" ~ 1,
+    year == "1980" ~ 0,
+    year == "1976" ~ 1,
+    year == "1972" ~ 1,
+    year == "1968" ~ 0,
+    year == "1964" ~ 0,
+    year == "1960" ~ 0,
+    year == "1956" ~ 1,
+    year == "1952" ~ 0,
+    year == "1948" ~ 0)) %>%
+  mutate(D_inc = case_when(
+    year == "2016" ~ 0,
+    year == "2012" ~ 1,
+    year == "2008" ~ 0,
+    year == "2004" ~ 0,
+    year == "2000" ~ 0,
+    year == "1996" ~ 1,
+    year == "1992" ~ 0,
+    year == "1988" ~ 0,
+    year == "1984" ~ 0,
+    year == "1980" ~ 1,
+    year == "1976" ~ 0,
+    year == "1972" ~ 0,
+    year == "1968" ~ 0,
+    year == "1964" ~ 1,
+    year == "1960" ~ 0,
+    year == "1956" ~ 0,
+    year == "1952" ~ 0,
+    year == "1948" ~ 1)) %>%
+  mutate(inc_pv2p = ifelse(D_inc == 1, D_pv2p, ifelse(R_inc == 1, R_pv2p, NA))) %>%
+  mutate(inc_pa2p_weighted = ifelse(D_inc == 1, D_pa2p_weighted, ifelse(R_inc == 1, R_pa2p_weighted, NA))) %>%
+  mutate(fmr_inc_pv2p = ifelse(D_inc == 1, fmr_D_pv2p, ifelse(R_inc ==1, fmr_R_pv2p, NA))) %>%
+  mutate(inc_net_approval_weighted = ifelse(is.na(inc_pv2p) == FALSE, net_approval_weighted, NA)) %>%
+  mutate(inc_party = ifelse(D_inc == 1, "Democrat", ifelse(R_inc == 1, "Republican", NA)))
 
+# Model Fitting
+
+# D_polls_model_df <- hist_natl %>%
+#   select(year, D_pv2p, D_pa2p_weighted) %>%
+#   drop_na()
+# 
+# D_polls_model <- train(D_pv2p ~ D_pa2p_weighted, 
+#                            data = D_polls_model_df, method = "lm", trControl = trainControl(method = "LOOCV"))
+
+R_polls_model_df <- hist_natl %>%
+  select(year, R_pv2p, R_pa2p_weighted) %>%
+  drop_na()
+
+R_polls_model <- train(R_pv2p ~ R_pa2p_weighted, 
+                       data = R_polls_model_df, method = "lm", trControl = trainControl(method = "LOOCV"))
+
+# D_polls_fmr_model_df <- hist_natl %>%
+#   select(year, D_pv2p, fmr_D_pv2p, D_pa2p_weighted) %>%
+#   drop_na()
+# 
+# D_polls_fmr_model <- train(D_pv2p ~ fmr_D_pv2p + D_pa2p_weighted, 
+#                          data = D_polls_fmr_model_df, method = "lm", trControl = trainControl(method = "LOOCV"))
+
+R_polls_fmr_model_df <- hist_natl %>%
+  select(year, R_pv2p, fmr_R_pv2p, R_pa2p_weighted) %>%
+  drop_na()
+
+R_polls_fmr_model <- train(R_pv2p ~ fmr_R_pv2p + R_pa2p_weighted, 
+                           data = R_polls_fmr_model_df, method = "lm", trControl = trainControl(method = "LOOCV"))
+
+# D_polls_fmr_demog_model_df <- hist_natl %>%
+#   select(year, D_pv2p, fmr_D_pv2p, D_pa2p_weighted, white_pct) %>%
+#   drop_na()
+# 
+# D_polls_fmr_demog_model <- train(D_pv2p ~ fmr_D_pv2p + D_pa2p_weighted + white_pct, 
+#                                  data = D_polls_fmr_demog_model_df, method = "lm", trControl = trainControl(method = "LOOCV"))
+
+R_polls_fmr_demog_model_df <- hist_natl %>%
+  select(year, R_pv2p, fmr_R_pv2p, R_pa2p_weighted, white_pct) %>%
+  drop_na()
+
+R_polls_fmr_demog_model <- train(R_pv2p ~ fmr_R_pv2p + R_pa2p_weighted + white_pct, 
+                                 data = R_polls_fmr_demog_model_df, method = "lm", trControl = trainControl(method = "LOOCV"))
+
+inc_df <- hist_natl %>%
+  select(year, inc_party, inc_pv2p, fmr_inc_pv2p, inc_pa2p_weighted, inc_net_approval_weighted)
+
+inc_polls_model_df <- inc_df %>%
+  select(year, inc_pv2p, inc_pa2p_weighted) %>%
+  drop_na()
+
+inc_polls_model <- train(inc_pv2p ~ inc_pa2p_weighted, 
+                   data = inc_polls_model_df, method = "lm", trControl = trainControl(method = "LOOCV"))
+
+inc_polls_approval_model_df <- inc_df %>%
+  select(year, inc_pv2p, inc_pa2p_weighted, inc_net_approval_weighted) %>%
+  drop_na()
+
+inc_polls_approval_model <- train(inc_pv2p ~ inc_pa2p_weighted + inc_net_approval_weighted, 
+                                  data = inc_polls_approval_model_df, method = "lm", trControl = trainControl(method = "LOOCV"))
+
+summary(inc_polls_approval_model)
+
+R_models <- tibble(model = c("R_polls_model", "R_polls_fmr_model", "R_polls_fmr_demog_model"))
+inc_models <- tibble(model = c("inc_polls_model", "inc_polls_approval_model"))
+
+R_loocv_results <- rbind(R_polls_model$results, R_polls_fmr_model$results, R_polls_fmr_demog_model$results)
+inc_loocv_results <- rbind(inc_polls_model$results, inc_polls_approval_model$results)
+
+R_loocv_results_table <- R_models %>% 
+  cbind(R_loocv_results) %>% 
+  tibble()
+inc_loocv_results_table <- inc_models %>% 
+  cbind(inc_loocv_results) %>% 
+  tibble()
+
+R_model_outputs <- export_summs(R_polls_model$finalModel, R_polls_fmr_model$finalModel, R_polls_fmr_demog_model$finalModel, 
+                              coefs = c("Intercept" = "(Intercept)",
+                                        "Republican Weighted Poll Avg." = "R_pa2p_weighted",
+                                        "Last Rep. 2 Party Pop. Vote" = "fmr_R_pv2p",
+                                        "White % of Pop." = "white_pct"),
+                              statistics = c(N = "nobs",
+                                             R2 = "r.squared",
+                                             R2.adj = "adj.r.squared",
+                                             sigma = "sigma"))
+
+quick_pdf(R_model_outputs, file = "graphics/R_national_models.pdf")
+
+inc_model_outputs <- export_summs(inc_polls_model$finalModel, inc_polls_approval_model$finalModel,
+                                coefs = c("Intercept" = "(Intercept)",
+                                          "Incumbent's Weighted Poll Avg." = "inc_pa2p_weighted",
+                                          "Incumbent's Weighted Net Approval" = "inc_net_approval_weighted"),
+                                statistics = c(N = "nobs",
+                                               R2 = "r.squared",
+                                               R2.adj = "adj.r.squared",
+                                               sigma = "sigma"))
+
+quick_pdf(inc_model_outputs, file = "graphics/inc_national_models.pdf")
 
 # State Data
 
